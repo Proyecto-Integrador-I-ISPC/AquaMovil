c94e610c82fabc50b1798c27a8c7bd0e6d1570c4
astridlup <astrita1970@gmail.com>
2025-06-05 12:56:15 -0300

    Agrego archivo pago.py para revisión

---
+++ pago.py
@@ -0,0 +1,10 @@
+from pedido import Pedidos
+
+class Pago:
+    def __init__(self, id_pago, pedido:Pedidos, tipo_pago, monto, fecha_pago, estado_pago):
+        self.id_pago = id_pago
+        self.id_pedido = pedido.id_pedido
+        self.tipo_pago = tipo_pago
+        self.monto = monto
+        self.fecha_pago = fecha_pago
+        self.estado_pago = estado_pago

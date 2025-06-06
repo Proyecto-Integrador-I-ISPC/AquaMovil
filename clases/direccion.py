199e71d3dfd33076cd61651a099189832ce2f6fd
Abigail <111169470+abipicone@users.noreply.github.com>
2025-06-06 09:41:19 -0300

    Revert "Código administrador completo."

--- clases/direccion.py
+++
@@ -1,9 +0,0 @@
-class Direcciones_empleado:
-    def __init__(self, id_direccion, provincia, localidad, barrio, calle, altura, piso='no_especificado'):
-        self.id_direccion = id_direccion
-        self.provincia = provincia
-        self.localidad = localidad
-        self.barrio = barrio
-        self.calle = calle
-        self.altura = altura
-        self.piso = piso
--- clases/servicio.py
+++
@@ -1,6 +0,0 @@
-class Servicio:
-    def __init__(self, id_servicio, tipo_servicio, descripcion, precio):
-        self.id_servicio = id_servicio
-        self.tipo_servicio = tipo_servicio
-        self.descripcion = descripcion
-        self.precio = precio
